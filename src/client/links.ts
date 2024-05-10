7d525de86ed63f08d49e8d383a94b843ddc24722
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2024-05-10 11:55:49 +0100

    hardcode api

--- src/client/links.ts
+++ src/client/links.ts
@@ -1,5 +1,5 @@
 export class ClientLinks {
-	api = process.env['API_URL'];
+	api = process.env['API_URL'] ?? ('https://chat-backend.tnfangel.com/api' as string);
 	cdn = process.env['CDN_URL'];
 	gateway = process.env['GATEWAY_URL'] ?? ('https://chat-backend.tnfangel.com' as string);
 	proxy = process.env['MEDIA_PROXY_URL'];
